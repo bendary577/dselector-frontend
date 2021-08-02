e076aba950775b848dcb991b0de723a9c55d51d0
cselection <mohamedyossif577@gmail.com>
2021-08-02 16:55:28 +0200

    zxczxc

--- src/components/buttons/MainButton.js
+++ src/components/buttons/MainButton.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import '../../assets/css/buttons/main_button.css';
 
 const MainButton = (props) => {
 
@@ -7,7 +8,7 @@ const MainButton = (props) => {
 
     return (
         <div className="button_div my-2">
-            <button>{props.title}</button>
+            <button className="main_button">{props.title}</button>
         </div >
     );
 }
